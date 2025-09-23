7b56cb1cc8b9ff55497ade8aac575d831cf0c5b9
kjetilnym0en <kjetilsn@uia.no>
2025-09-23 17:21:27 +0200

    Forsøk 1

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-from random import random, randint, uniform
+from random import random, randint, uniform 
 
-count = 0
 
-for _ in range(1, 6):
-    d1 = randint(1, 100)
-guess1 = int(input("Guess a number: "))
-count += 1
-if guess1 == d1:
-    print("Congratulations! You guessed the number in {count} attempt(s).")
-elif guess1 < d1:
-    print("The number is higher")
-    newguess = int(input("Guess again: "))
-elif guess1 > d1:
-    print("The number is lower")
-    newguess = int(input("Guess again: "))
+answer1 = "YES" 
+answer2 = "NO" 
+
+answer = "YES"
+ 
+while answer == answer1:
+    d1 = randint(1, 100) 
+    count = 0 
+    while count < 5: 
+        guess1 = int(input("Guess a number: ")) 
+        count += 1 
+        
+        if guess1 == d1: 
+            print("Congratulations! You guessed the number in", count, "attempt(s).")
+            break
+        
+        elif guess1 < d1: 
+            print("The number is higher") 
+        
+        else:
+            print("The number is lower") 
+        
+        if count == 5: print("Sorry! You did not manage to guess the number. You have reached the guessing limit. The number was:", d1) 
     
+    answer = str(input("Do you want to play again? Type 'YES' or 'NO'. ")).upper()
+else:
+    print("Okey, see you again.")
